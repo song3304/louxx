28520555166a233bd3ae837daf02e1db480d273f
fly <fly@load-page.com>
2015-10-09 11:27:46 +0800

    fix wechat

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -3,15 +3,15 @@ namespace App\Http\Controllers;
 
 use Addons\Core\Controllers\WechatController as BaseWechatController;
 use Addons\Core\Models\WechatUser;
-use Addons\Core\Models\Wechat\API;
-use Addons\Core\Models\Wechat\User as WechatUserModel;
+use Addons\Core\Models\Tools\API;
+use Addons\Core\Models\Tools\User as WechatUserTool;
 
 class WechatController extends BaseWechatController {
 
 	protected function user(API $api, WechatUser $wechatUser)
 	{
 		//如果不希望加入到系统的用户表，请注释下行
-		(new WechatUserModel($api))->bindToUser($wechatUser);
+		(new WechatUserTool($api))->bindToUser($wechatUser);
 	}
 
 }
