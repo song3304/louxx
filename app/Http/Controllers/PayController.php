9f3ed9e6ebdf04b1f7fc04f51cdaf43e1599bff5
fly <fly@load-page.com>
2015-10-27 18:01:41 +0800

    fix

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -30,7 +30,7 @@ class PayController extends WechatOAuth2Controller
 		$api = new API($account->toArray(), $account->getKey());
 
 		$pay = new Pay($api);
-		$order = (new UnifiedOrder('JSAPI', date('YmdHis'), '买单', 1))->SetAttach('说点什么好?')->SetNotify_url(url('wechat/feedback?id='.$account->getKey()))->SetOpenid($wechatUser->openid);
+		$order = (new UnifiedOrder('JSAPI', date('YmdHis'), '买单', 1))->SetAttach('说点什么好?')->SetNotify_url(url('wechat/feedback').'?id='.$account->getKey())->SetOpenid($wechatUser->openid);
 		$UnifiedOrderResult = $pay->unifiedOrder($order);
 		
 		$js = new Js($api);
