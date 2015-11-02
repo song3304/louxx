adad9138237757e672475bdc75fcea83bba2ced1
fly <fly@load-page.com>
2015-11-02 17:37:15 +0800

    fix

--- config/session.php
+++ config/session.php
@@ -109,7 +109,7 @@ return [
     |
     */
 
-    'cookie' => 'laravel_session',
+    'cookie' => md5(config('app.path')).'_session',
 
     /*
     |--------------------------------------------------------------------------
