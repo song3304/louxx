eff6ad687d5810966f94ca10bacee963129671dd
fly <fly@load-page.com>
2016-01-07 19:04:52 +0800

    fix plugins

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      *
      * @var array
      */
-    protected $commands = [
+    public $commands = [
         \App\Console\Commands\Inspire::class,
     ];
 
