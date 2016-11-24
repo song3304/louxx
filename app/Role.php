42989dff59d96203a1db4aa255c2c2701531bd60
fly <fly@load-page.com>
2016-11-24 16:49:54 +0800

    move getAll to Tree.php

--- app/Role.php
+++ app/Role.php
@@ -2,8 +2,24 @@
 namespace App;
 
 use Addons\Entrust\Role as BaseRole;
-
+use Addons\Core\Models\TreeCacheTrait;
+use Illuminate\Support\Arr;
 class Role extends BaseRole
 {
+	use TreeCacheTrait;
 	
+	public static function getRolesByName($name = NULL)
+	{
+		empty(static::$cacheTree) && static::getAll('name');
+		$name = str_replace(['.', '.children.children', '.children.children'], ['.children.', '.children', '.children'], $name);
+		return is_null($name) ? static::$cacheTree['name'] : 
+			(empty($name) || in_array($name, ['none', 'null']) ? static::find(0)->toArray() : Arr::get(static::$cacheTree['name'], $name));
+	}
+
+	public static function getRolesById($id = NULL)
+	{
+		empty(static::$cacheTree) && static::getAll();
+		return is_null($id) ? static::$cacheTree['id'][ 0 ][ 'children' ] : 
+			(empty($id) ? static::find(0)->toArray() : Arr::get(static::$cacheTree['id'], $id));
+	}
 }
