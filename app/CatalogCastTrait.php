6f66d4172d98c56c351782c5d4e545fde89950a1
fly <fly@load-page.com>
2016-11-25 19:07:46 +0800

    fix CatalogCastTrait.php

--- app/CatalogCastTrait.php
+++ app/CatalogCastTrait.php
@@ -5,11 +5,11 @@ use App\Catalog;
 trait CatalogCastTrait {
 	public function asCatalog($value) {
 		$data = Catalog::getCatalogsById($value);
-		return new Catalog($data);
+		return (new Catalog())->setRawAttributes($data);
 	}
 	public function asCatalogName($value) {
 		$data = Catalog::getCatalogsByName($value);
-		return new Catalog($data);
+		return (new Catalog())->setRawAttributes($data);
 	}
 	public function asStatus($value) {
 		return $this->asCatalog($value);
