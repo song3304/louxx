42355b2a51ae3c4b581fc4662a2d7f9ab66f7994
fly <fly@load-page.com>
2016-12-07 19:35:37 +0800

    add from to CatalogCastTrait.php

--- app/CatalogCastTrait.php
+++ app/CatalogCastTrait.php
@@ -8,12 +8,26 @@ trait CatalogCastTrait {
 		unset($data['children']);
 		return (new Catalog())->setRawAttributes($data);
 	}
+
+	public function fromCatalog($value) {
+		return $value->toArray();
+	}
+
 	public function asCatalogName($value) {
 		$data = Catalog::getCatalogsByName($value);
 		unset($data['children']);
 		return (new Catalog())->setRawAttributes($data);
 	}
+
+	public function fromCatalogName($value) {
+		return $value->toArray();
+	}
+
 	public function asStatus($value) {
 		return $this->asCatalog($value);
+	}
+
+	public function fromStatus($value) {
+		return $value->toArray();
 	}
 }
