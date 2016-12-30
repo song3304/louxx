c1a740fa9efd3c19c3babaf0b2850ea400aa5113
fly <fly@load-page.com>
2016-12-31 01:19:11 +0800

    upgrade elastic

--- database/migrations/2100_12_31_235959_seeds.php
+++ database/migrations/2100_12_31_235959_seeds.php
@@ -59,6 +59,7 @@ class Seeds extends Migration
 				'title' => '无'
 			])->update(['id' => 0]);
 			DB::statement("ALTER TABLE `catalogs` AUTO_INCREMENT = 11;");
+			DB::statement("UPDAE `catalogs` SET `path` = '/0/' WHERE `id` = 0;");
 
 			$fields = [
 				'gender|性别' => [
