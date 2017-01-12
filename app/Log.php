42e263ee39abf301c4ac3ed4b3ad62a769415879
fly <fly@load-page.com>
2017-01-12 11:30:12 +0800

    fix

--- app/Log.php
+++ app/Log.php
@@ -72,9 +72,10 @@ class Log extends Auditing
 		!empty($data['new']) && $data['new'] = json_encode($data['new']);
 		!empty($data['old']) && $data['old'] = json_encode($data['old']);
 
-		$data['request']['server'] = array_only($data['request']['server'], ['HTTP_HOST', 'HTTP_SCHEME', 'HTTPS', 'HTTP_CONNECTION', 'CONTENT_LENGTH', 'HTTP_ORIGIN', 'HTTP_X_CSRF_TOKEN', 'CONTENT_TYPE', 'HTTP_ACCEPT', 'HTTP_X_REQUESTED_WITH', 'HTTP_REFERER', 'HTTP_ACCEPT_ENCODING', 'HTTP_ACCEPT_LANGUAGE', 'HTTP_COOKIE', 'SERVER_SIGNATURE', 'SERVER_SOFTWARE', 'SERVER_NAME', 'SERVER_ADDR', 'SERVER_PORT', 'REMOTE_ADDR', 'DOCUMENT_ROOT', 'REQUEST_SCHEME', 'CONTEXT_PREFIX', 'CONTEXT_DOCUMENT_ROOT', 'SCRIPT_FILENAME', 'REMOTE_PORT', 'REDIRECT_URL', 'GATEWAY_INTERFACE', 'SERVER_PROTOCOL', 'REQUEST_METHOD', 'QUERY_STRING', 'REQUEST_URI', 'SCRIPT_NAME', 'PHP_SELF', 'REQUEST_TIME_FLOAT', 'REQUEST_TIME', 'FCGI_ROLE', 'REDIRECT_STATUS']);
-		foreach($data['request'] as $k => &$v)
-			$k !== 'server' && $v = json_encode($v);
+		is_array($data['request']['server']) && $data['request']['server'] = array_only($data['request']['server'], ['HTTP_HOST', 'HTTP_SCHEME', 'HTTPS', 'HTTP_CONNECTION', 'CONTENT_LENGTH', 'HTTP_ORIGIN', 'HTTP_X_CSRF_TOKEN', 'CONTENT_TYPE', 'HTTP_ACCEPT', 'HTTP_X_REQUESTED_WITH', 'HTTP_REFERER', 'HTTP_ACCEPT_ENCODING', 'HTTP_ACCEPT_LANGUAGE', 'HTTP_COOKIE', 'SERVER_SIGNATURE', 'SERVER_SOFTWARE', 'SERVER_NAME', 'SERVER_ADDR', 'SERVER_PORT', 'REMOTE_ADDR', 'DOCUMENT_ROOT', 'REQUEST_SCHEME', 'CONTEXT_PREFIX', 'CONTEXT_DOCUMENT_ROOT', 'SCRIPT_FILENAME', 'REMOTE_PORT', 'REDIRECT_URL', 'GATEWAY_INTERFACE', 'SERVER_PROTOCOL', 'REQUEST_METHOD', 'QUERY_STRING', 'REQUEST_URI', 'SCRIPT_NAME', 'PHP_SELF', 'REQUEST_TIME_FLOAT', 'REQUEST_TIME', 'FCGI_ROLE', 'REDIRECT_STATUS']);
+		if (is_array($data['request']))
+			foreach($data['request'] as $k => &$v)
+				$k !== 'server' && $v = json_encode($v);
 		return $data;
 	}
 
@@ -107,4 +108,9 @@ Log::creating(function($log){
 		$log->device = $agent->device();
 	}
 	
+});
+
+Log::created(function($log){
+	if (!in_arary($log->type, ['created', 'updated', 'deleted', 'saved', 'restored', ]))
+		event('log.'.$log->type, [$log]);
 });
