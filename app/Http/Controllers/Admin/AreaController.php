049f790eb895a4ebd85ce3fb731d827a3f6c6e27
songhui <284606700@qq.com>
2018-10-23 21:40:07 +0800

    省市区处理

--- app/Http/Controllers/Admin/AreaController.php
+++ app/Http/Controllers/Admin/AreaController.php
@@ -18,9 +18,17 @@ class AreaController extends Controller
 		
 		$total = $this->_getCount($request, $builder, FALSE);
 		$builder = $builder->orderBy('area_id','asc');
+		
 		$data = $this->_getData($request, $builder, null);
-		$data['recordsTotal'] = $total; //不带 f q 条件的总数
-		$data['recordsFiltered'] = $data['total']; //带 f q 条件的总数
+		$inputs = $request->input('f', []);
+		if($inputs['parent_id']===''){
+		    $data['data'] = [];
+		    $data['recordsTotal'] = 0;
+		    $data['recordsFiltered'] = 0;
+		}else{
+		    $data['recordsTotal'] = $total; //不带 f q 条件的总数
+		    $data['recordsFiltered'] = $data['total']; //带 f q 条件的总数
+		}
 		return $this->api($data);
 	}
 }
